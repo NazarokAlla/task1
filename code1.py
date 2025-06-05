2689cb30959444136b40b1597407b05937d962c3
NazarokAlla <nazarokalla@gmail.com>
2025-06-05 15:22:00 +0300

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,12 @@
+# Ask for user's name
+name = input("What is your name? ")
+
+# Greet the user
+print("Hello, " + name + "!")
+
+# Ask a question about nesting principles
+answer = input("How many nesting principles are you know? ")
+
+# Respond based on the answer
+print("Interesting! You answered: " + answer)
+print("There are 7 key testing principles")
