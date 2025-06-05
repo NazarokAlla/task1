703ea5c57d9ef3f6d514101f2ca9acf93317dd3b
NazarokAlla <nazarokalla@gmail.com>
2025-06-05 16:53:10 +0300

    Update code1.py

--- code1.py
+++ code1.py
@@ -10,3 +10,10 @@ answer = input("How many nesting principles are you know? ")
 # Respond based on the answer
 print("Interesting! You answered: " + answer)
 print("There are 7 key testing principles")
+
+# Ask for first testing principle
+ans = input("What is first testing principle?")
+
+# Respond based on the answer
+print("Ok, You answered: " + ans)
+print("The first key testing principle is - Testing shows presence of errors")
